653a608f4f5894ab06bf6d71a3e7992d1bd8f234
Argragas <avezard.jeason@gmail.com>
2019-01-22 20:52:50 +0100

    u

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import random
+
+playerhp = 260
+enemyatk_min = 60
+enemyatk_max = 80
 
 
-def my_function(num1, num2):
-    return num1 + num2
+while playerhp > 0:
+    damage = random.randrange(enemyatk_min, enemyatk_max)
+    playerhp = playerhp - damage
+    
+    if playerhp <= 30:
+        playerhp = 30
+        
+        
+    print("Dommage :", damage, "points de vie restant :", playerhp)
+
+    if playerhp > 30:
+        continue
+        
+    print("point de vie bas")
+    break
 
 
-print(my_function(5, 5))
